e4e0663b0561fed1b926de6f8d0962a5ece783ea
20060516 <pavithra.p2023ece@sece.ac.in>
2025-07-14 11:41:56 +0530

    Day 09 task-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Login from "./components/Login"
+import Register from "./components/Register"
+//import './index.css';
 
 function App() {
   return(
     <div>
       <Login />
+      <Register />
+      <h1>Welcome to the Application</h1>
+      <p>Please login or register to continue.</p>
     </div>
   )
 }

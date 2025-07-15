1a26590d46782cbdb69415ea84cb3933c90f15f4
20060516 <pavithra.p2023ece@sece.ac.in>
2025-07-15 20:01:59 +0530

    Day 11 A.N React

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import Login from "./components/Login"
 import Register from "./components/Register"
-//import './index.css';
+import React from "react";
+import Signup from "./components/Signup";
+
 
 function App() {
   return(
     <div>
+    <h1><center>Welcome to the Application</center></h1>
+    <p>Please login or register to continue.</p>
+    <Signup/>
+    <Register />
       <Login />
-      <Register />
-      <h1>Welcome to the Application</h1>
-      <p>Please login or register to continue.</p>
+      
+      
     </div>
   )
 }
